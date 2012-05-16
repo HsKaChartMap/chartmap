c2feca5976187f3f91c13ba0c81adc18cef0c674
romanjw <roman.woessner@freenet.de>
2012-05-16 17:49:27 +0200

    changed datasource of 'staaten'-layer to php output

--- js/mappanel.js
+++ js/mappanel.js
@@ -27,20 +27,12 @@ Ext.application({
 			{layers: 'bmng'}, {transitionEffect: 'null'}
 		);
 		
-		var states = new OpenLayers.Layer.Vector("Staaten", {
-			projection: new OpenLayers.Projection("EPSG:4326"),
-        	protocol: new OpenLayers.Protocol.HTTP({				
-						url: "data/staaten.json",
-						format: new OpenLayers.Format.GeoJSON()
-	    		})
-		});
-		
 		
 		var staaten = new OpenLayers.Layer.Vector("Staaten", {
        			strategies: [new OpenLayers.Strategy.BBOX()],	
 			projection: new OpenLayers.Projection("EPSG:4326"),
         		protocol: new OpenLayers.Protocol.HTTP({				
-						url: "data/staaten.json",
+						url: "php/getJSON.php?key=tyadrylIpQ1K_iHP407374Q",
 						format: new OpenLayers.Format.GeoJSON()
 	    		})
 		});
