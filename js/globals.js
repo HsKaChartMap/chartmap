94a8bba1b0bbfa80101b9cf3eb8b60c74e8cbed4
romanjw <roman.woessner@freenet.de>
2012-06-13 18:42:45 +0200

    added selectedFeatures and restructured globals.js

--- js/globals.js
+++ js/globals.js
@@ -1,10 +1,22 @@
 // define global vars here
 
+// OpenLayers.Map object
 var map;
+
+// Dynamic country feature store
 var countryFS;
+
+// Indicator store to populate the ComboBox
 var indicatorStore;
+
+// ComboBoxes
 var indComboBox;
 var yearComboBox;
 var clTypeComboBox;
 var clComboBox;
+
+// Vector Legend
 var vectorLegend;
+
+// Array containing selected features
+var selectedFeatures = new Array();
