1d365ddc07475326bf102b612dbc89d2402f5617
romanjw <roman.woessner@freenet.de>
2012-06-15 09:25:40 +0200

    Amr's code

--- js/getYearsByIndicator.js
+++ js/getYearsByIndicator.js
@@ -1,7 +1,7 @@
 /* Function getYearsByIndicator
  * Parameters:
- *      layername: string
- *      indicator: string
+ * layername: string
+ * indicator: string
  * Returns a store which contains the available years of the given indicator
  */
 function getYearsByIndicator(layername, indicator) {
@@ -9,25 +9,38 @@ function getYearsByIndicator(layername, indicator) {
     var matchingLayers;
     var vectorLayer;
     var yearStore;
+    var index = -1;
+    var dates;
     
     // get vector layer
     matchingLayers = map.getLayersByName(layername);
     if (matchingLayers.length == 1) {
         vectorLayer = matchingLayers[0];
+        dates = new Array();
     }
     else {
         console.log("getThematicStyleMap: Warning, the layer " + layername + " was not found!");
         return;
     }
-    
+
     for (var i=0;i < vectorLayer.features.length;i++) {
         if (vectorLayer.features[i]['data'][indicator]) {
-            // extract keys here!
-        }
-    }
+             //if (vectorLayer.features[i]['data'][indicator].length > 1) {
+            var x = vectorLayer.features[i]['data'][indicator].length;
+            for (var j=0; j < x; j++) {
+                if (vectorLayer.features[i]['data'][indicator][j] != "") {
+                    index = index + 1;
+                    dates[index] = vectorLayer.features[i]['data'][indicator][j];
+                }// end if
+            }// enf for
+        }// end if
+    }// end for
 
     //build store containing the extracted keys
-    
+    yearStore = new Ext.data.SimpleStore({
+        fields:['year'],
+        data: dates
+    });
     //return the store
     return yearStore;
 }
