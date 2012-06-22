6411404b0c3573dca3bc44b03eec47b5e3270d39
romanjw <roman.woessner@freenet.de>
2012-06-22 07:40:28 +0200

    Amr's scaling

--- js/scales.js
+++ js/scales.js
@@ -2,5 +2,8 @@
 const scale_funs = {
     "NO_SCALING" : function(v) { return v; },
     "SCALE_TIMES_100" : function(v) { return v*100; },
+    "SCALE_DIVIDE_100" : function(v) { return v/100; },
+	"SCALE_DIVIDE_1000" : function(v) { return v/1000; },
+    "SCALE_DIVIDE_10000" : function(v) { return v/10000; }
     "SCALE_RELATIVE": function(v,max) {return 100*(v/max);}
 }
